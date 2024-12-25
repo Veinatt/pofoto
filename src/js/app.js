912ed74a22da8ecba66c95ad9c7c738357f8d686
Veinatt <denmuraviov20@gmail.com>
2024-12-26 00:20:08 +0300

    2612 0020

--- src/js/app.js
+++ src/js/app.js
@@ -19,9 +19,9 @@ function ready(fn) {
 }
 
 
-AOS.init({
-  duration: 1000,
-});
+// AOS.init({
+//   duration: 1000,
+// });
 
 
 ready(function () {
@@ -114,321 +114,57 @@ var toggleHeader = function () {
 
 window.addEventListener('scroll', checkScroll);
 
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
-
-const rangeInput = document.querySelectorAll(".range-input input"),
-  priceInput = document.querySelectorAll(".price-input input"),
-  range = document.querySelector(".slider .progress");
-let priceGap = 1000;
-
-priceInput.forEach((input) => {
-  input.addEventListener("input", (e) => {
-
-    let minPrice = parseInt(priceInput[0].value),
-      maxPrice = parseInt(priceInput[1].value);
-
-    if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInput[1].max) {
-      if (e.target.className === "input-min") {
-        rangeInput[0].value = minPrice;
-        range.style.left = (minPrice / rangeInput[0].max) * 100 + "%";
-      } else {
-        rangeInput[1].value = maxPrice;
-        range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
-      }
-    }
-  });
-});
-
-rangeInput.forEach((input) => {
-  input.addEventListener("input", (e) => {
-    let minVal = parseInt(rangeInput[0].value),
-      maxVal = parseInt(rangeInput[1].value);
-
-    if (maxVal - minVal < priceGap) {
-      if (e.target.className === "range-min") {
-        rangeInput[0].value = maxVal - priceGap;
-      } else {
-        rangeInput[1].value = minVal + priceGap;
-      }
-    } else {
-      priceInput[0].value = minVal;
-      priceInput[1].value = maxVal;
-      range.style.left = (minVal / rangeInput[0].max) * 100 + "%";
-      range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
-    }
-  });
-});
-
-document.querySelectorAll(".product-counter").forEach(product_counter => {
-  let counter = 0;
-
-  const counterValue = product_counter.querySelector('.counter-value');
-  const incrementBtn = product_counter.querySelector('.increment-btn');
-  const decrementBtn = product_counter.querySelector('.decrement-btn');
-
-  // To increment the value of counter
-  incrementBtn.addEventListener('click', () => {
-    counter++;
-    counterValue.innerHTML = counter;
-    if (counter > 1) {
-      decrementBtn.disabled = false
-    }
-  });
-
-  // To decrement the value of counter
-  decrementBtn.addEventListener('click', () => {
-      counter--;
-      counterValue.innerHTML = counter;
-      if (counter == 1) {
-        decrementBtn.disabled = true
-      }
-  });
-});
-
-
-// var swiper = new Swiper('.partners-swiper', {
-//   spaceBetween: 0,
-//   centeredSlides: true,
-//   speed: 3000,
-//   autoplay: {
-//     delay: .001,
-//   },
-//   loop: true,
-//   slidesPerView: 'auto',
-//   allowTouchMove: false,
-//   disableOnInteraction: true
-// });
-
-
-
-// window.addEventListener("DOMContentLoaded", function() {
-//   [].forEach.call( document.querySelectorAll('.TelRu'), function(input) {
-//     var keyCode;
-//     function mask(event) {
-//       event.keyCode && (keyCode = event.keyCode);
-//       var pos = this.selectionStart;
-//       if (pos < 3) event.preventDefault();
-//       var matrix = "+7 (___) ___ ____",
-//           i = 0,
-//           def = matrix.replace(/\D/g, ""),
-//           val = this.value.replace(/\D/g, ""),
-//           new_value = matrix.replace(/[_\d]/g, function(a) {
-//               return i < val.length ? val.charAt(i++) : a
-//           });
-//       i = new_value.indexOf("_");
-//       if (i != -1) {
-//           i < 5 && (i = 3);
-//           new_value = new_value.slice(0, i)
-//       }
-//       var reg = matrix.substr(0, this.value.length).replace(/_+/g,
-//           function(a) {
-//               return "\\d{1," + a.length + "}"
-//           }).replace(/[+()]/g, "\\$&");
-//       reg = new RegExp("^" + reg + "$");
-//       if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
-//         this.value = new_value;
-//       }
-//       if (event.type == "blur" && this.value.length < 5) {
-//         this.value = "";
-//       }
-//     }
-
-//     input.addEventListener("input", mask, false);
-//     input.addEventListener("focus", mask, false);
-//     input.addEventListener("blur", mask, false);
-//     input.addEventListener("keydown", mask, false);
-
-//   });
-
-// });
-
-window.addEventListener('scroll', function () {
-  let y = document.documentElement.scrollTop;
-  if (y > 10) {
-    document.querySelector('header').classList.add('active');
-  } else {
-    document.querySelector('header').classList.remove('active');
+const swiper_review = new Swiper('.swiper-review', {
+  spaceBetween: 20,
+  loop: true,
+  breakpoints: {
+    1000: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    800: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
   }
-})
-
-
-
-// const swiper_examples = new Swiper('.swiper-examples', {
-//   loop: true,
-//   navigation: {
-//     nextEl: '.swiper-examples-next',
-//     prevEl: '.swiper-examples-prev',
-//   },
-//   breakpoints: {
-//     // when window width is >= 640px
-//     1025: {
-//       slidesPerView: 3,
-//       spaceBetween: 20,
-//     },
-//     550: {
-//       slidesPerView: 2,
-//       spaceBetween: 20,
-//     },
-//     280: {
-//       slidesPerView: 1,
-//       spaceBetween: 0,
-//     }
-//   }
-// });
-// const swiper_review = new Swiper('.swiper-review', {
-//   loop: true,
-//   navigation: {
-//     nextEl: '.swiper-review-next',
-//     prevEl: '.swiper-review-prev',
-//   },
-//   breakpoints: {
-//     // when window width is >= 640px
-//     1400: {
-//       slidesPerView: 3,
-//       spaceBetween: 20,
-//     },
-//     800: {
-//       slidesPerView: 2,
-//       spaceBetween: 20,
-//     },
-//     550: {
-//       slidesPerView: 1,
-//       spaceBetween: 0,
-//     }
-//   }
-// });
-
-
-
-// var linkToggle = document.querySelectorAll('.js-toggle');
-
-// for (let i = 0; i < linkToggle.length; i++) {
-
-//   linkToggle[i].addEventListener('click', function (event) {
-
-//     event.preventDefault();
-
-//     var container = document.getElementById(this.dataset.container);
-
-
-//     if (!container.classList.contains('active')) {
-//       if (this.classList.contains('req-btn')) {
-//         this.textContent = 'Скрыть'
-//       }
-//       if (this.classList.contains('req-btn1')) {
-//         this.textContent = 'Скрыть'
-//       }
-
-//       this.classList.add('active');
-//       container.classList.add('active');
-//       container.style.height = 'auto';
-
-//       var height = container.clientHeight + 'px';
-
-//       container.style.height = '0px';
-
-//       setTimeout(function () {
-//         container.style.height = height;
-//       }, 0);
-//     } else {
-//       this.classList.remove('active');
-//       if (this.classList.contains('req-btn')) {
-//         this.textContent = 'Другие услуги'
-//       }
-//       if (this.classList.contains('req-btn')) {
-//         this.textContent = 'Показать ещё'
-//       }
-//       container.style.height = '0px';
-
-//       container.addEventListener('transitionend', function () {
-//         container.classList.remove('active');
-//       }, {
-//         once: true
-//       });
-
-//     }
-
-//   });
-
-// }
-// // Close the dropdown menu if the user clicks outside of it
-// window.onclick = function (event) {
-//   if (!event.target.matches('.dropbtn')) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('active')) {
-//         openDropdown.style.height = '0px';
-
-//         openDropdown.addEventListener('transitionend', function () {
-//           openDropdown.classList.remove('active');
-//         }, {
-//           once: true
-//         });
-//       }
-//     }
-//   }
-// }
-
-// if (document.querySelector('.animated-cont')) {
-//   document.querySelectorAll('.animated-cont').forEach(cont => {
-//     var animate_delay = window.innerHeight - cont.dataset.delayOnTop;
-//     var rect = cont.getBoundingClientRect();
-
-//     var block_top = rect.top + document.documentElement.scrollTop + animate_delay;
-//     var element_animated = false;
-
-//     function animationOnScroll() {
-//       if (!element_animated && document.documentElement.scrollTop + window.innerHeight > block_top) {
-//         element_animated = true;
-//         if (cont.querySelector('.sc-cont')) {
-//           cont.querySelectorAll('.point').forEach(item => {
-//             item.classList.add("animate");
-//           }, false);
-//         }
-//       }
-//     }
-
-//     ready(animationOnScroll());
-//     window.addEventListener('scroll', (event) => {
-//       animationOnScroll()
-//     })
-//   }, false);
-// };
-// const swiper_review = new Swiper('.swiper-review', {
-//   navigation: {
-//     nextEl: '.swiper-review-next',
-//     prevEl: '.swiper-review-prev',
-//   },
-//   spaceBetween: 20,
-//   loop: true,
-//   breakpoints: {
-//     1000: {
-//       slidesPerView: 2.5,
-//       spaceBetween: 20,
-//     },
-//     800: {
-//       slidesPerView: 2,
-//       spaceBetween: 20,
-//     },
-//     300: {
-//       slidesPerView: 1,
-//       spaceBetween: 20,
-//     },
-//   }
-// });
+});
+const swiper_addon = new Swiper('.swiper-addon', {
+  spaceBetween: 20,
+  loop: true,
+  breakpoints: {
+    1000: {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+    800: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+  }
+});
+const swiper_price = new Swiper('.swiper-price', {
+  spaceBetween: 20,
+  loop: true,
+  breakpoints: {
+    1000: {
+      slidesPerView: 6,
+      spaceBetween: 20,
+    },
+    800: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+  }
+});
